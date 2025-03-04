7f8cc5930f250d0a2c26031ea732ed3b4939dd09
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-04 19:41:53 +0000

    1.2 Complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,35 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-	
+	public static boolean validIndex(int[] array, int num){
+		boolean answer= true;
+		try{
+			array[num] = 0;
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			answer=false;
+		}
+		return answer;
+	}
+	public static int divide(int num1, int num2){
+		int result=1;
+		try{
+			result= num1/num2;
+		}
+		catch (Exception e){
+			result =0;
+		}
+		return result;
+	}
+	public static int safeConvertStringtoInt(String word){
+		int a=1;
+		try{
+			a= Integer.parseInt(word);
+		}
+		catch( Exception e){
+			a=0;
+		}
+		return a;
+	}
 
 }
